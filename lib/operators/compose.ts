8126f991c1508a4eeeefee36004007cd10e9dc8d
Carson McKinstry <carson.mckinstry@trivago.com>
2019-10-29 11:07:02 +0100

    Removed compose and pipe, as those already exist

--- lib/operators/compose.ts
+++
@@ -1,9 +0,0 @@
-import { Transform, PassThrough } from "stream";
-
-export const compose = (...ops: Transform[]) => {
-    const ps = new PassThrough({ objectMode: true });
-    ops.reverse().forEach(op => {
-        ps.pipe(op);
-    });
-    return ps;
-};
--- lib/operators/pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Transform, PassThrough } from "stream";
-
-export const pipe = (...ops: Transform[]) => {
-    const ps = new PassThrough({ objectMode: true });
-    ops.forEach(op => {
-        ps.pipe(op);
-    });
-    return ps;
-};
