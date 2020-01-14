1677dc1fdaef6962f61aac2911dd60f10c7547d0
Carson McKinstry <carson.mckinstry@trivago.com>
2020-01-14 10:40:00 +0100

    fixed a minor typing issue

---
+++ src/creators/index.ts
@@ -0,0 +1,4 @@
+export { of } from "./of";
+export { from } from "./from";
+export { interval } from "./interval";
+export { range } from "./range";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-export * from "./operators";
-export * from "./combinators";
+export {
+    map,
+    filter,
+    reduce,
+    scan,
+    sink,
+    tap,
+    skip,
+    skipFirst,
+    mergeMap,
+    flatMap,
+    concatMap,
+    switchMap
+} from "./operators";
+export { join, innerJoin, merge } from "./combinators";
+export { of, from, interval, range } from "./creators";
